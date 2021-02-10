d13ceb2645cedb1e4f184072f3476643a15539c8
fodorpapbalazsdev <fodorpapbalazsdev@gmail.com>
2021-02-10 17:24:44 +0100

    plannableItem cleanup

--- src/main/java/models/PlannableItem.java
+++ src/main/java/models/PlannableItem.java
@@ -15,14 +15,15 @@ public class PlannableItem extends Item {
 
     public PlannableItem(Map<String, Object> fields, String plannableFieldName) throws Exception {
         super(fields);
+
         Object fieldValue = fields.get(plannableFieldName);
         if (fieldValue == null) {
             throw new FieldNotFoundException(plannableFieldName);
-        } else if (fieldValue instanceof Date) {
-            Date plannedDate = (Date) fields.get(plannableFieldName);
-            this.plannedDate = plannedDate;
-        } else {
+        }
+        if (!(fieldValue instanceof Date)) {
             throw new Exception("The given field with name: " + plannableFieldName + " is not a Date.");
+        } else {
+            this.plannedDate = (Date) fields.get(plannableFieldName);;
         }
     }
 }
--- src/test/java/models/PlannableItemTest.java
+++ src/test/java/models/PlannableItemTest.java
@@ -4,8 +4,6 @@ import exceptions.FieldNotFoundException;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.sql.Date;
 import java.util.HashMap;
@@ -16,29 +14,37 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("PlannableItemTest")
 class PlannableItemTest {
 
-    @SneakyThrows
+    final Date testDate = Date.valueOf("2021-02-20");
+
     @Test
+    @SneakyThrows
+    @DisplayName("PlannableItem constructor should create a PlannableItem")
     public void createPlannableItem_shouldCreate() {
         Map<String, Object> fields = new HashMap<>();
-        Date date = Date.valueOf("2021-02-20");
-        fields.put("PCD", date);
+        fields.put("PCD", testDate);
+        fields.put("Summary", "Lorem ipsum");
+        fields.put("Assigned User", "Dummy Username");
         PlannableItem plannableItem = new PlannableItem(fields, "PCD");
-        assertTrue(plannableItem.getPlannedDate().equals(date));
+        assertEquals(testDate, plannableItem.getPlannedDate());
+        assertEquals(plannableItem.getFields().size(), 3);
+        assertEquals(plannableItem.getFields().get("Assigned User"), "Dummy Username");
     }
 
-    @SneakyThrows
     @Test
+    @SneakyThrows
+    @DisplayName("createPlannableItem should throw FieldNotFoundException when field not found")
     public void createPlannableItem_shouldThrowException() {
         Map<String, Object> fields = new HashMap<>();
         fields.put("PCD", "2021-02-20");
         assertThrows(Exception.class, () -> new PlannableItem(fields, "PCD"));
     }
 
-    @SneakyThrows
     @Test
-    public void createPlannableItem_shouldThrowException_2() {
+    @SneakyThrows
+    @DisplayName("createPlannableItem should throw FieldNotFoundException when field not found")
+    public void createPlannableItemShouldThrowExceptionWhenGivenFieldNotFound() {
         Map<String, Object> fields = new HashMap<>();
-        fields.put("Due Date", Date.valueOf("2021-02-20"));
+        fields.put("Due Date", testDate);
         assertThrows(FieldNotFoundException.class, () -> new PlannableItem(fields, "PCD"));
     }
 
